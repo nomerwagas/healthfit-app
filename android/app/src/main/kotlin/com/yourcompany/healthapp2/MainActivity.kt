2df356fde9625e484b229496e7df0a726e060658
Nomer <nomerwagas3794@gmail.com>
2026-03-27 11:33:55 +0800

    fix:enabled the biometric

--- android/app/src/main/kotlin/com/yourcompany/healthapp2/MainActivity.kt
+++ android/app/src/main/kotlin/com/yourcompany/healthapp2/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.yourcompany.healthapp2
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
